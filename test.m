2e2150acd5eed81db39e6605978ce45a5eaee01b
sakura0711 <testuseroozx@gmail.com>
2023-06-01 02:21:04 +0800

    Update

--- test.m
+++ test.m
@@ -23,3 +23,29 @@ for i = 0:0:16
 end
 disp(randisVision);
 disp('s');
+
+%% 
+% 26 是日期
+datestr(now,26);
+% 13 是時間
+datestr(now,13);
+
+%% 
+% 创建一个图形窗口和文本对象来显示时间
+figure;
+timeText = uicontrol('Style', 'text', 'Position', [100 100 200 50], 'FontSize', 24);
+
+% 创建一个计时器对象
+t = timer('ExecutionMode', 'fixedRate', 'Period', 1, 'TimerFcn', @(~,~)updateTime());
+
+% 启动计时器
+start(t);
+
+% 定义计时器回调函数
+function updateTime()
+    currentTime = clock;  % 获取当前时间
+    
+    % 将时间转换为字符串并在文本对象中显示
+    timeString = sprintf('%02d:%02d:%02d', currentTime(4), currentTime(5), fix(currentTime(6)));
+    set(timeText, 'String', timeString);
+end
